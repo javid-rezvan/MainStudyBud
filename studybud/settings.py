235df63006b2d7f984177a146f38b8079dee602f
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-05-22 16:33:34 +0430

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             BASE_DIR /'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR /'static'
+]
+MEDIA_URL='/images/'
+MEDIA_ROOT=BASE_DIR /'static/images'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

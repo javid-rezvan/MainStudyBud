fbe9e787c090ffe4c1f20554149f9f3205931110
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-05-22 17:39:41 +0430

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . models import User,Topic,Room,Message
 
 def home(request):
-    return render(request,'base/home.html')
+    topics=Topic.objects.all()
+    context={'topics':topics}
+    return render(request,'base/home.html',context)
 
 
 

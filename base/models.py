20a76493245a5a0adce058fc9597005fa4266dcc
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-05-22 15:08:53 +0430

    Update models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    name=models.CharField(max_length=200)
+    bio=models.TextField(null=True)
+    avatar=models.ImageField(default='avatar.svg',null=True)
+    email=models.EmailField(unique=True,null=True)
+    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username']
+
+
+

671521f9d977e90ad8da3a9d44dfb266467e384c
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-05-23 13:16:29 +0430

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from . models import User,Topic,Room,Message
+from django.db.models import Q
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q')!=None else ''
     topics=Topic.objects.all()
-    rooms=Room.objects.filter(topic__name__icontains=q)
-    context={'topics':topics,'rooms':rooms}
+    rooms=Room.objects.filter(Q(topic__name__icontains=q) |
+                              Q(name__icontains=q)|
+                              Q(host__username__icontains=q)) 
+    room_messages=Message.objects.filter(room__topic__name__icontains=q)
+    context={'topics':topics,'rooms':rooms,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 

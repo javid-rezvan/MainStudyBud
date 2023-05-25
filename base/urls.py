558c890c9bfb117243a0e21a042b73b4008a0b05
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-05-25 17:33:42 +0430

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -13,7 +13,8 @@ urlpatterns =[
     path('signup/',views.registerUser,name='signup'),
     path('topics/',views.topics,name='topics'),
     path('activity/',views.activityPage,name='activity'),
-    path('delete-room/<str:pk>/',views.deleteRoom,name='delete-room')
+    path('delete-room/<str:pk>/',views.deleteRoom,name='delete-room'),
+    path('delete-message/<str:pk>/',views.deleteMessage,name='delete-message')
 ]
 
 
